dfcdf36a16c758c1ec2802d805ac9b6d28608566
Squall Leonhart <knight1988@gmail.com>
2023-11-23 13:42:57 +0700

    update build setup circle ci

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-docker build -t knight1988/node-serverless:18 -t knight1988/node-serverless:latest . && \
-docker push knight1988/node-serverless:18 && \
-docker push knight1988/node-serverless:latest
+docker build -t knight1988/node-serverless:18 . && \
+docker push knight1988/node-serverless:18
 
